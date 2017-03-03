60379f0195f29e996d77c3ebec11442f3c8c410c
LBCC Library <libref@linnbenton.edu>
2017-03-03 13:03:55 -0800

    Adding more configuration options

---
+++ lib/traject/globe.config.rb
@@ -0,0 +1,6 @@
+require 'traject'
+
+to_field 'record_provider_facet', literal('Drama Online Library')
+to_field 'record_source_facet', literal('Globe on Screen')
+to_field 'is_electronic_facet', literal('Online')
+to_field 'format', literal('Streaming video')
---
+++ lib/traject/gvrl.config.rb
@@ -0,0 +1,7 @@
+require 'traject'
+
+to_field 'record_provider_facet', literal('Gale Virtual Reference Library')
+to_field 'record_source_facet', literal('Gale Virtual Reference Library')
+to_field 'is_electronic_facet', literal('Online')
+to_field 'format', literal('Ebook')
+to_field 'url_fulltext_display', extract_marc('856u') # GVRL links come with the Proxy Prefix appended
