25d4b0a89f693288e60c10bce3f399f60b5680fc
Jane Sandberg <sandbej@linnbenton.edu>
2017-07-13 09:33:17 -0700

    rescuing network errors during authorities indexing

--- lib/traject/eg_authorities.config.rb
+++ lib/traject/eg_authorities.config.rb
@@ -8,18 +8,21 @@ to_field 'authority_data_t' do |record, accumulator|
 		authorized_subfields.each do |subfield|
 			control_no = subfield.value.delete("^0-9")
 			eg_authority_url = "http://libcat.linnbenton.edu/opac/extras/supercat/retrieve/marcxml/authority/#{control_no}"
-			open(eg_authority_url) do |ar|
-				reader = MARC::XMLReader.new(ar)
-				for arecord in reader
-					interesting_authority_fields = arecord.find_all {|f| ['400', '410', '411', '430', '447', '448', '450', '451', '455', '500', '510', '511', '530', '550', '551', '555', '663', '664', '665', '666', '680', '681', '682', '700', '710', '711', '730', '747', '748', '750', '751', '755', '780', '781', '782'].include? f.tag}
-					interesting_authority_fields.each do |afield|
-						afield.each do |asubfield|
-							unless '0' == asubfield.code
-								accumulator << asubfield.value
+			begin
+				open(eg_authority_url) do |ar|
+					reader = MARC::XMLReader.new(ar)
+					for arecord in reader
+						interesting_authority_fields = arecord.find_all {|f| ['400', '410', '411', '430', '447', '448', '450', '451', '455', '500', '510', '511', '530', '550', '551', '555', '663', '664', '665', '666', '680', '681', '682', '700', '710', '711', '730', '747', '748', '750', '751', '755', '780', '781', '782'].include? f.tag}
+						interesting_authority_fields.each do |afield|
+							afield.each do |asubfield|
+								unless '0' == asubfield.code
+									accumulator << asubfield.value
+								end
 							end
 						end
 					end
 				end
+			rescue
 			end
 		end
 	end
