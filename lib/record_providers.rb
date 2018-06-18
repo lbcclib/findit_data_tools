c8ec172376efcdf487cab77f249627249a16badd
Jane Sandberg <sandbergja@users.noreply.github.com>
2018-06-18 10:09:10 -0700

    Add JoMI

--- lib/record_providers.rb
+++ lib/record_providers.rb
@@ -7,6 +7,13 @@ module FindItData
                 'fetch_url' => 'http://open.umn.edu/opentextbooks/MARC/OTL20161101.mrc',
                 'file_prefix' => 'opentextbooks',
                 'traject_configuration_files' => ['lib/traject/opentextbooks.config.rb'],
+                },
+            'jomi' => {
+                'record_provider_facet' => 'JoMI Surgical Videos',
+                'fetch_method' => 'http',
+                'fetch_url' => 'https://jomi.com/jomiRecords.mrc',
+                'file_prefix' => 'jomi',
+                'traject_configuration_files' => ['lib/traject/opentextbooks.config.rb','lib/traject/proxy.config.rb'],
                 }
         }
     end
